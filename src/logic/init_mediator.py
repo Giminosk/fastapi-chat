8f533f41d9e123a0c0b6aa9b31ac400da02c5a82
Giminosk <giminosk@gmail.com>
2024-05-08 14:58:52 +0300

    added readme

--- src/logic/init_mediator.py
+++ src/logic/init_mediator.py
@@ -1,8 +1,6 @@
-from domain.values.chat import Title
 from logic.commands.chat import CreateChatCommand, CreateChatCommandHandler
 from logic.mediator import Mediator
 from repositories.base import BaseChatRepository
-from repositories.memory import MemoryChatRepository
 
 
 def init_mediator(mediator: Mediator, chat_repository: BaseChatRepository):
