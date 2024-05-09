26064e965fc8eea6d2bf3b66774af5ca16094892
Giminosk <giminosk@gmail.com>
2024-05-09 13:45:32 +0300

    merged main and app

--- src/app/main.py
+++ src/app/main.py
@@ -1,8 +1,20 @@
 import uvicorn
 
+from fastapi import FastAPI
+
+
+def create_app() -> FastAPI:
+    app = FastAPI(
+        title="FastAPI Chat",
+        docs_url="/api/v1/docs",
+        debug=True,
+    )
+    return app
+
+
 if __name__ == "__main__":
     uvicorn.run(
-        "app:create_app",
+        "main:create_app",
         factory=True,
         host="0.0.0.0",
         port=8000,
